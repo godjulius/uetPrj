caa87383aa3d3100b0294db48cc0af9cbac6b14d
duongnp <duongnp28072003@gmail.com>
2025-04-07 23:37:19 +0700

    Learning page

--- src/app/pages/courses/course-lesson/course-lesson.component.ts
+++ src/app/pages/courses/course-lesson/course-lesson.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {LandingFooterComponent} from '../../landing/landing-footer/landing-footer.component';
+import {TabsModule} from 'primeng/tabs';
 
 @Component({
   selector: 'app-course-lesson',
   standalone: true,
-  imports: [],
+    imports: [
+        LandingFooterComponent,
+        TabsModule
+    ],
   templateUrl: './course-lesson.component.html',
   styleUrl: './course-lesson.component.css'
 })
