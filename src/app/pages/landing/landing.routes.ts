8412d0ce0548de7fc60ed3cfa55ea335651006ae
duongnp <duongnp28072003@gmail.com>
2025-04-09 11:21:41 +0700

    instructor UI

--- src/app/pages/landing/landing.routes.ts
+++ src/app/pages/landing/landing.routes.ts
@@ -24,7 +24,7 @@ export const routes: Routes = [
                 component: CoursePreviewComponent
             },
             {
-                path: 'user/:username',
+                path: 'instructor/:username',
                 component: InstructorComponent
             }
         ]
